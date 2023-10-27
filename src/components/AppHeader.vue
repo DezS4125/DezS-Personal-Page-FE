a843488b77bb62258344efd2a6003129c7278a14
DezS4125 <khangb2014748@student.ctu.edu.vn>
2023-10-27 08:45:35 +0700

    Initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+	<nav class="navbar navbar-expand navbar-dark bg-dark">
+		<a
+			href="/"
+			class="navbar-brand"
+			>Ứng dụng Quản lý danh bạ</a
+		>
+		<div class="mr-auto navbar-nav">
+			<li  class="nav-item">
+				<router-link to="/" class="nav-link">Home</router-link>
+			</li>
+			<li>
+				<router-link to="/about" class="nav-link">About</router-link>
+			</li>
+			<li>
+				<router-link to="/contact" class="nav-link">Contact</router-link>
+			</li>
+		</div>
+	</nav>
+</template>
